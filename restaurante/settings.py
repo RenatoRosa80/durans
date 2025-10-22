1948d0b89794fc2f58bb663e58ec22d63cf57dd7
CLEYTON DURANS DO NASCIMENTO <cleyton.nascimento@aluno.senac.rio>
2025-10-22 19:21:24 -0300

    atualizando

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -128,3 +128,8 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 DATE_INPUT_FORMATS = ['%d/%m/%Y']
+
+# Adiciona o diretório 'static' na raiz do projeto como local de busca
+STATICFILES_DIRS = [
+    BASE_DIR / 'static' 
+]
