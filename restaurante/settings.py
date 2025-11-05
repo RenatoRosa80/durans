d4dfc084da45db0548c7cee6e19d2efbf45a57c4
Yasmin <cleytondurans.n@gmail.com>
2025-11-05 00:18:58 -0300

    durans

--- restaurante/settings.py
+++ restaurante/settings.py
@@ -17,6 +17,11 @@ from pickle import FALSE
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+# Configurações para upload de imagens (MEDIA)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
@@ -125,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
+# Configuração de arquivos estáticos
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
